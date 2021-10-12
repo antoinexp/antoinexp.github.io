6e009a212234aad030eaab1a6dfcbd96488bddfc
antoinexp <antoinexp@gmail.com>
2021-10-12 20:20:20 +0200

    guitar poc

---
+++ poc_guitar/precache-manifest.19e16feb2f854c0f71e10c234d2f18e7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "005a1e63df528d689cc1cb4253f6dddc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4c1227a53a0bb4bdffb6",
+    "url": "/static/css/main.5e3647ae.chunk.css"
+  },
+  {
+    "revision": "b6d07d4aba29888140f7",
+    "url": "/static/js/2.5519f94a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5519f94a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c1227a53a0bb4bdffb6",
+    "url": "/static/js/main.bb04d508.chunk.js"
+  },
+  {
+    "revision": "d722166b52f389ead02e",
+    "url": "/static/js/runtime-main.82a50b8d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
